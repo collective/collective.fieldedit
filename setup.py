d3daa3b84c2bdd4f34ba972e5bc1714210d03d60
Philip Bauer <bauer@starzel.de>
2018-08-04 15:30:16 +0200

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.fieldedit',
-    version='1.0a2',
+    version='1.0a3.dev0',
     description="A flexible form to edit selected fields of a content type.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
