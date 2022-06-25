82f358cf52d62e2cb8213c75f3df54dba496e317
Philip Bauer <bauer@starzel.de>
2022-06-25 11:40:04 +0200

    update classifiers

--- setup.py
+++ setup.py
@@ -11,8 +11,11 @@ setup(
     description="A flexible form to edit selected fields of a content type.",
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
         "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: Addon",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
@@ -28,13 +31,19 @@ setup(
     keywords="Python Plone",
     author="Philip Bauer",
     author_email="bauer@starzel.de",
-    url="https://pypi.python.org/pypi/collective.fieldedit",
+    url="https://github.com/collective/collective.fieldedit",
+    project_urls={
+        "PyPI": "https://pypi.python.org/pypi/collective.fieldedit",
+        "Source": "https://github.com/collective/collective.fieldedit",
+        "Tracker": "https://github.com/collective/collective.fieldedit/issues",
+    },
     license="GPL version 2",
     packages=find_packages("src", exclude=["ez_setup"]),
     namespace_packages=["collective"],
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires=[
         # -*- Extra requirements: -*-
         "plone.api",
